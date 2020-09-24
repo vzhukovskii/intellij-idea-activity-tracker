3ab04be665c673bd1dc1646ae547955d8c374ddb
GitHub Action <action@github.com>
2020-09-24 09:56:10 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/vzhukovskii/intellijideaactivitytracker/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.vzhukovskii.intellijideaactivitytracker.services
+
+import com.github.vzhukovskii.intellijideaactivitytracker.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/vzhukovskii/intellijideaactivitytracker/services/MyProjectService.kt
+++ src/main/kotlin/com/github/vzhukovskii/intellijideaactivitytracker/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.vzhukovskii.intellijideaactivitytracker.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vzhukovskii.intellijideaactivitytracker.MyBundle
 
 class MyProjectService(project: Project) {
 
